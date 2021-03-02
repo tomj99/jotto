4edc52ced8b42dcb681cec022f8dee66cbc92e06
tjacques <tjacques.ctr@kr.af.mil>
2021-03-02 18:25:29 -0500

    added proptypes testing

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,21 +1,47 @@
 import React from "react";
-
-export default (props) => {
-      if (props.success) {
-        return (
-        <div data-test="component-congrats">
-          <span data-test="congrats-message">
-            Congrats Bitch! You Guessed the Word!
-          </span>
-        </div>
-        );
-      } else {
-        return (
-            <div data-test="component-congrats" />
-        );
-      }
+import PropTypes from "prop-types";
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+    <div data-test="component-congrats">
+      <span data-test="congrats-message">
+        Congrats Bitch! You Guessed the Word!
+      </span>
+    </div>
+    );
+  } else {
+    return (
+        <div data-test="component-congrats" />
+    );
+  }
 }
 
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+
+};
+
+export default Congrats;
+
+//if you don't need a name for adding props you can do this
+// export default (props) => {
+//     if (props.success) {
+//         return (
+//             <div data-test="component-congrats">
+//           <span data-test="congrats-message">
+//             Congrats Bitch! You Guessed the Word!
+//           </span>
+//             </div>
+//         );
+//     } else {
+//         return (
+//             <div data-test="component-congrats" />
+//         );
+//     }
+// }
+
+
+//older way to do this
 // function Congrats (props) {
 //   if (props.success) {
 //     return (
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-
 import Congrats from "./Congrats";
 
-import { findByTestAttr } from "../test/setupTests";
+import { findByTestAttr, checkProps } from "../test/setupTests";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false};
+
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<Congrats {...setupProps} />);
 };
 
 //renders without error
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -32,3 +34,8 @@ test("renders non-empty congrats message when success prop is true", () => {
   const component = findByTestAttr(wrapper, "congrats-message");
   expect(component.text().length).not.toBe(0);
 });
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false};
+  checkProps(Congrats, expectedProps);
+})
