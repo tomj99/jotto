e5f548b58181472585b88c9ea413f3fb5160c7d8
tjacques <tjacques.ctr@kr.af.mil>
2021-03-03 12:48:45 -0500

    added code to setupTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 import Congrats from "./Congrats";
 
 import { findByTestAttr, checkProps } from "../test/setupTests";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false};
 
--- test/setupTests.js
+++ test/setupTests.js
@@ -1,4 +1,8 @@
 import checkPropTypes from "check-prop-types";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 // sets up test-data attributes
 export const findByTestAttr = (wrapper, val) =>
